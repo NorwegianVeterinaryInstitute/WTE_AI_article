3e998aaa84a9af93383a5f77bf2d3754c1b08ef9
Eve Zeyl Fiskebeck <evezeyl@gmail.com>
2024-03-08 09:57:20 +0100

    was also forgotten

---
+++ code/R/AI_dependencies.R
@@ -0,0 +1,29 @@
+# R packages on CRAN
+library(tidyverse)
+library(rlist)
+# library(pipeR) # need to try if works without this
+library(xlsx)
+library(glue)
+
+library(ggplot2)
+library(ggnewscale)
+library(scales)
+library(qualpalr)
+library(RColorBrewer)
+library(ggplotify)
+library(ggforce)
+#library(patchwork)
+#library(plotly)
+# library(phyloseq)  # tyr without this one
+library(phytools)
+library(rlang)
+library(ape)
+# R packages on Bioconductor
+library(ggtree)
+library(ggtreeExtra)
+# R packages to help programming
+#library(usethis)
+
+
+# Used in R scripts 
+#library(seqinr)
